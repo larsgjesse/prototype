901cd7160c6f987d0be1509cf44364266b57a462
Thomas Sejerøe <ths@topsil.com>
2018-10-13 09:56:52 +0200

    Added option for the v1 version of the Python code to use a file to write / read from - this should enable cross runtime and version testing.

--- binary/python/baseline.py
+++ binary/python/baseline.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
 import messages_pb2
+import os.path
+from optparse import OptionParser
 
 def sendMessage():
     print('Packaging message')
@@ -33,12 +35,58 @@ def receiveMessage(packedMessage):
         
         for linkMeasurement in staResults.linkmeasurements:
             print('---> Saw BSSID: ' + str(linkMeasurement.BSSID) + ' at ' + str(linkMeasurement.rssi) + ' dB')
+            
+def writeBinaryDataToFile(binaryData, filename):
+    with open(os.getcwd() + "/" + filename, 'wb') as outputFile:
+        print('-> Writing to: ' + filename)
+        outputFile.write(binaryData)
+        
+def readBinaryDataFromFile(filename):
+    with open(os.getcwd() + "/" + filename, 'rb') as inputFile:
+        print('-> Reading from: ' + filename)
+        binaryData = inputFile.read()
+        
+    return binaryData
 
+def main(options):
+    print('Base line protobuf send/receive test, V1')
+    print('----------------------------------------')
+    
+    if (options.mode == "1"):
+        print('Mode 1 - direct memory transfer.')
+        packedMessage = sendMessage()
+        receiveMessage(packedMessage)
+    
+    if (options.mode == "2"):
+        print('Mode 2 - transfer via intermediate file.')
+        print('-> File: ' + options.filename)
+        packedMessage = sendMessage()
+        writeBinaryDataToFile(packedMessage, options.filename)
+        readMessage = readBinaryDataFromFile(options.filename)
+        receiveMessage(readMessage)
+    
+    if (options.mode == "3"):
+        print('Mode 3 - read existing file.')
+        print('-> File: ' + options.filename)
+        if (os.path.exists(options.filename)):
+            readMessage = readBinaryDataFromFile(options.filename)
+            receiveMessage(readMessage)
+        else:
+            print('Could not find specified file!')
+    
+    print('----------------------------------------')
+    
 if __name__ == "__main__":
-    print('Base line protobuf send/receive test')
-    print('----------------------------------------')
-    packedMessage = sendMessage()
-    print('      |\n      v')
-    receiveMessage(packedMessage)
-    print('----------------------------------------')
+   parser = OptionParser()
+   
+   parser.add_option("-f", "--file", dest="filename",
+                     help="use this file to read from / write to.", metavar="FILE",
+                     default="message_v1.bin")	
+   parser.add_option("-m", "--mode", dest="mode",
+                     help="test mode: 1 = direct, 2 = via file, 3 = only FROM file.", metavar="MODE",
+                     default=1)
+   
+   (options, args) = parser.parse_args()
+   main(options)
+
     
